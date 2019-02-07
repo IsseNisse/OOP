3cfb3a38fb372c348fd409199df6b5f706d959fe
IsseNisse <isak.holmstrom@elev.ga.ntig.se>
2019-02-07 11:14:49 +0100

    Gjorde om metoden för multiplikation så att den stämde överens med räknereglerna

--- src/KomplexaTal.java
+++ src/KomplexaTal.java
@@ -17,8 +17,10 @@ public class KomplexaTal {
     public KomplexaTal multiply(KomplexaTal z2) {
         double a = this.a * z2.a;
         double b = this.b * z2.b;
-        KomplexaTal z4 = new KomplexaTal(a,b);
-        return z4;
+        double c = this.a * z2.b;
+        double d = this.b * z2.a;
+        KomplexaTal z3 = new KomplexaTal(a-b,c+d);
+        return z3;
     }
 
         public String toString() {
